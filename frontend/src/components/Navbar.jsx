2d4b819485d52933c321aa7ac4a5ec6b82b944bf
Tey <justinevillanueva090794@gmail.com>
2022-11-17 17:34:23 +0800

    Banner

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {Container ,Button , Form , Nav} from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
+import { BsCartPlus } from "react-icons/bs";
+import '../CSS/Navbar.css'
+
+const Navbar1 = () => {
+  return (
+    <div>
+    <Navbar collapseOnSelect expand="lg" variant="dark" style={{background: '#DEF5E5', height: '70px'}}>
+        <Container>
+            <Navbar.Brand href="#home" className='text-dark fw-bold'>UNDERGRNDEV</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" style={{background: 'teal'}}/>
+            <Navbar.Collapse id="responsive-navbar-nav" className='button1'>
+
+          <Nav className="me-auto">
+            <Nav.Link className='text-dark' href="#/">Home</Nav.Link>
+            <Nav.Link className='text-dark' href="#about">About</Nav.Link>
+            <Nav.Link className='text-dark' href="#product">Product</Nav.Link>
+          </Nav>
+
+          <Form className="d-flex">
+            <Form.Control type="search" placeholder="Search"className="me-2"
+            aria-label="Search"/>
+            <Button variant="outline-success">Search</Button>
+          </Form>
+
+          <Nav>
+            <Nav.Link className='text-dark' href="#register">Register</Nav.Link>
+            <Nav.Link className='text-dark' eventKey={2} href="#login">
+              Login
+            </Nav.Link>
+            <Nav.Link className='text-dark' href="#register"><BsCartPlus className='cart'/></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>
+  )
+}
+
+export default Navbar1
---
+++ frontend/src/pages/Banner.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Container , Row , Col} from 'react-bootstrap';
+import {bannerItems} from '../data'
+import '../CSS/Banner.css'
+
+
+const Banner = () => {
+  return (
+    <div className='banner1'>
+        
+    <Container>
+    {bannerItems.map((item) => (
+        <Row>
+            <Col sm={6}><img src={item.img1} alt=""style={{height: '70%'}}/></Col>
+            <Col className='text-center text-bold' 
+                 style={{marginTop: '20%'}} sm={6}>
+                    <span style={{fontSize: '70px'}}>{item.title1}</span>
+                    <br />
+                    <span style={{fontSize: '20px',letterSpacing: '3px'}}>{item.desc1}</span>
+                    <br/>
+                    <button className='btn1'>SHOP NOW</button>
+                 </Col>
+        </Row>
+         ))}       
+    </Container>
+
+    </div>
+  )
+}
+
+export default Banner
